d756172493a3605dbbd01f0953dcce3493f5ae6a
diaz26 <jeffreydiazaya@outlook.com>
2019-04-08 18:08:03 -0500

    .

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -9,6 +9,8 @@ class Home extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('view_home');
+    $this->load->view('header');
+    $this->load->view('view_home');
+		$this->load->view('footer');
 	}
 }
---
+++ application/views/view_home.php
@@ -0,0 +1,5 @@
+<body>
+  <div class="content">
+    hola
+  </div>
+</body>

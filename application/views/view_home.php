48fea0ac12a407dc5502108f6dfe3c022d5bb246
diaz26 <jeffreydiazaya@outlook.com>
2019-04-16 23:54:41 -0500

    .

--- application/views/view_home.php
+++ application/views/view_home.php
@@ -18,6 +18,7 @@
   <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,700" rel="stylesheet">
   <!--== CSS FILES ==-->
   <link href="<?= base_url(); ?>assets/css/event.css" rel="stylesheet" />
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
   <!--[if lt IE 9]>
@@ -38,9 +39,6 @@
 -->
 <body>
   <!-- Preloader -->
-  <div id="preloader">
-    <div id="status">&nbsp;</div>
-  </div>
 
   <section>
     <div class="land-full land-3-top-full">
@@ -67,18 +65,19 @@
             <img src="<?= base_url(); ?>assets/images/banner.jpg" alt="" class="land-1-pro-img">
           </div>
 
-          <div class="land-3-dwn-right">
+          <div class="land-3-dwn-right" style="background-color: #F8F8F8">
             <h2 style="color:#999999">INTIIV</h2>
-            <div class="land-container">
-              <div class="land-3-dwn">
-                <div class="land-3-dwn-right">
-                  <p style="color:#C7C7C7">ES EL PRIMER PRODUCTO PORTABLE PARA LA HIGIENE ÍNTIMA FEMENINA EN EL MUNDO.</p>
-                </div>
+            <div class="row">
+              <div class="col-lg-5 col-md-5 col-xs-5 col-12">
+
               </div>
+              <div class="col-lg-7 col-md-7 col-xs-7 col-12">
+                <p style="color:#C7C7C7">ES EL PRIMER PRODUCTO PORTABLE PARA LA HIGIENE ÍNTIMA FEMENINA EN EL MUNDO.</p>
+              </div>
+
             </div>
 
-
-            <p style="color:#AFAFAF">CON SU FÓRMULA BASADA EN TECNOLOGIA NATURAL MANTIENE UNA HIGIENE Y PROTECCION ÓPTIA DE NUESTRA ZONA INTIMA, BALANCEA SU PH, PREVIENE EL MAL OLOR Y DISMINUYE EL FLUJO</p>
+            <p style="color:#AFAFAF;background-color: #F1F1F1">CON SU FÓRMULA BASADA EN TECNOLOGIA NATURAL MANTIENE UNA HIGIENE Y PROTECCION ÓPTIA DE NUESTRA ZONA INTIMA, BALANCEA SU PH, PREVIENE EL MAL OLOR Y DISMINUYE EL FLUJO</p>
             <p style="color: #76C2C8">HIGIENE INSTANTÁNEA SI ENJUAGUE.</p>
           </div>
         </div>
@@ -90,69 +89,112 @@
     <div class="land-full land-4-agen-full">
       <div class="land-container">
         <div class="land-3-agen">
-          <div class="land-3-agen-left">
-            <h2 style="color:#999999">MODO SE USO</h2>
+          <div class="row">
+            <div class="col-lg-6 col-md-6 col-xs-6 col-12">
+              <div class="land-3-agen-left">
+                <h4 style="color:#999999">MODOS DE USO</h4>
+              </div>
+              <div class="land-3-agen-right">
+                <ul>
+                  <li>
+                    <p style="color:#AFAFAF">APLICA MIIKA INTIIV EN CUALQUIER OCASIÓN OPORTUNA EN LA ZONA V PARA MAYOR PROTECCIÓN E HIGIENE.</p>
+                  </li>
+                  <li>
+                    <p style="color:#AFAFAF">NO REQUIERE ENJUAGUE.</p>
+                  </li>
+                  <li>
+                    <p style="color:#AFAFAF">NO CUENTA CON UN LÍMITE DE APLICACIONES DIARIAS.</p>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-lg-6 col-md-6 col-xs-6 col-12" style="background-color: #F8F8F8">
+              <section>
+                <div class="land-full">
+                  <div class="row">
+                    <div class="col-lg-5 col-md-5 col-xs-5 col-5">
+                      <h5 style="color:#999999">COMPONENTES</h5>
+                    </div>
+                    <div class="col-lg-7 col-md-7 col-xs-7 col-7">
+                      <P style="color:#C5907B">
+                        TECNOLOGIA 100% NATURAL - PH BALANCEADO 5.5.
+                      </p>
+                      <P style="color:#C5907B">
+                        EXTRACTOS NATURALES: MANZANILLA Y ALOE VERA.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+
+              </section>
+
+              <section>
+                <div class="land-full">
+                  <div class="land-3-agen" style="background-color: #F1F1F1">
+                    <div class="row">
+                      <div class="col-lg-5 col-md-5 col-xs-5 col-5">
+                        <h5 style="color:#999999">BENEFICIOS</h5>
+                      </div>
+                      <div class="col-lg-7 col-md-7 col-xs-7 col-7">
+                        <P style="color:#C5907B">
+                          BALANCEA EL PH
+                        </p>
+                        <P style="color:#C5907B">
+                          PREVIENE EL MAL HOLOR
+                        </p>
+                        <P style="color:#C5907B">
+                          DISMINUYE EL FLUJO
+                        </p>
+                        <P style="color:#C5907B">
+                          AUMENTA LA SENSACION DE LIMPIEZA Y FRESCURA
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </section>
+            </div>
           </div>
-          <div class="land-3-agen-right">
-            <ul>
-              <li>
-                <p style="color:#AFAFAF">APLICA MIIKA INTIIV EN CUALQUIER OCASIÓN OPORTUNA EN LA ZONA V PARA MAYOR PROTECCIÓN E HIGIENE.</p>
-              </li>
-              <li>
-                <p style="color:#AFAFAF">NO REQUIERE ENJUAGUE.</p>
-              </li>
-              <li>
-                <p style="color:#AFAFAF">NO CUENTA CON UN LÍMITE DE APLICACIONES DIARIAS.</p>
-              </li>
-            </ul>
+          <div class="row">
+
+            <div class="col-lg-12 col-md-12 col-xs-12 col-12">
+              <center><h3 style="color:#999999;padding-bottom: 9px">OCASIONES DE CONSUMO</h3></center>
+              <div class="land-3-agen-left">
+                <h5 style="color:#C5907B">EN CUALQUIER OCASION OPORTUNA</h5>
+                <h5 style="color:#C5907B">EN CUALQUIER MOMENTO, EN CUALQUIER LUGAR</h5>
+              </div>
+              <div class="land-3-agen-right">
+                <ul>
+                  <li>
+                    <p style="color:#AFAFAF">ANTES Y DESPUES DE CUALQUIER EXPOSICIÓN</p>
+                  </li>
+                  <li>
+                    <p style="color:#AFAFAF">ANTES Y DESPUES DE TENER RELACIONES SEXUALES</p>
+                  </li>
+                  <li>
+                    <p style="color:#AFAFAF">ANTES Y DESPUES DE IR A LA PISCINA O LA PLAYA</p>
+                  </li>
+                  <li>
+                    <p style="color:#AFAFAF">ANTES Y DESPUES DE IR AL BAÑO</p>
+                  </li>
+                  <li>
+                    <p style="color:#AFAFAF">DESPUES DE REALIZAR CUALQUIER ACTIVIDAD FÍSICA</p>
+                  </li>
+                  <li>
+                    <p style="color:#AFAFAF">EN NUESTROS VIAJES</p>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
           </div>
         </div>
       </div>
+
     </div>
   </section>
 
-  <section>
-    <div class="land-full land-3-cus-full">
-      <div class="land-container">
-        <div class="land-3-cus-tit">
-          <h2>Customers says</h2>
-          <p>Aenean eu cursus sapien. Quisque eu ultricies ligula.</p>
-        </div>
-        <div class="land-3-cus-box">
-          <div class="test-box">
-            <img src="<?= base_url(); ?>assets/images/cus1.png">
-            <p>Proin felis massa, dapibus ut porta vitae, viverra nec dolor. Sed non aliquet nisl. Donec mollis, lorem ac vulputate feugiat, magna lacus mollis justo, eu pretium ipsum arcu et velit.</p>
-            <h4>Matthew</h4>
-            <h6>New York</h6>
-          </div>
-        </div>
-        <div class="land-3-cus-box">
-          <div class="test-box">
-            <img src="<?= base_url(); ?>assets/images/cus2.png">
-            <p>Proin felis massa, dapibus ut porta vitae, viverra nec dolor. Sed non aliquet nisl. Donec mollis, lorem ac vulputate feugiat, magna lacus mollis justo, eu pretium ipsum arcu et velit.</p>
-            <h4>Matthew</h4>
-            <h6>New York</h6>
-          </div>
-        </div>
-        <div class="land-3-cus-box">
-          <div class="test-box">
-            <img src="<?= base_url(); ?>assets/images/cus3.png">
-            <p>Proin felis massa, dapibus ut porta vitae, viverra nec dolor. Sed non aliquet nisl. Donec mollis, lorem ac vulputate feugiat, magna lacus mollis justo, eu pretium ipsum arcu et velit.</p>
-            <h4>Matthew</h4>
-            <h6>New York</h6>
-          </div>
-        </div>
-        <div class="land-3-cus-box">
-          <div class="test-box">
-            <img src="<?= base_url(); ?>assets/images/cus4.png">
-            <p>Proin felis massa, dapibus ut porta vitae, viverra nec dolor. Sed non aliquet nisl. Donec mollis, lorem ac vulputate feugiat, magna lacus mollis justo, eu pretium ipsum arcu et velit.</p>
-            <h4>Matthew</h4>
-            <h6>New York</h6>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
+
 
 
   <!-- LOCATION -->
@@ -189,6 +231,9 @@
   <script src="<?= base_url(); ?>assets/js/main.min.js"></script>
   <script src="<?= base_url(); ?>assets/js/custom.js"></script>
   <script src="<?= base_url(); ?>assets/js/mail.js"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   <script>
   (function(i, s, o, g, r, a, m) {
     i['GoogleAnalyticsObject'] = r;
